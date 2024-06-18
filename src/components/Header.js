5bbdcf5334e93ece14dac08391d35a294dd9388e
Gustavo-Aal <gustavomarquesaal@gmail.com>
2024-06-18 16:16:52 -0300

    Atualizado o cabeçalho com o novo tema e logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,37 @@
 import Link from "next/link";
+import House from "./icons/House";
+import Duck from "./icons/Duck";
+import Logo from "./Logo";
 
 
 export default function Header(){
     return (
         <header
         className="
-        flex justify-around
-        bg-base-300
+        flex gap-8 pl-4 pt-1
+        items-center
+        bg-neutral
         ">
             <Link href="/"
-            className="btn btn-primary text-neutral-content">
-                Home
+            className="btn btn-link no-underline text-neutral-content">
+                <Logo/>
+                {/* <House className="fill-"/> */}
             </Link>
+
             <Link href="/introducao"
-            className="btn btn-primary text-neutral-content">
-                Comece <strong>aqui</strong>!
+            className="text-neutral-content font-bold">
+                Comece aqui!
             </Link>
             <Link href="/html"
-            className="btn btn-primary text-neutral-content">
+            className="text-neutral-content">
                 HTML
             </Link>
             <Link href="/css"
-            className="btn btn-primary text-neutral-content">
+            className="text-neutral-content">
                 CSS
             </Link>
             <Link href="/javascript"
-            className="btn btn-primary text-neutral-content">
+            className="text-neutral-content">
                JavaScript
             </Link>
         </header>
