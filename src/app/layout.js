12b4e95e7de0d366050e76603fb931b129737ff7
Gustavo-Aal <gustavomarquesaal@gmail.com>
2024-06-19 13:33:26 -0300

    Corrigido hydration error no layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,15 +18,14 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
       </head>
-      <body className={inter.className}>
-        <div className="
+      <body className={`
+        ${inter.className}
         flex flex-col
         bg-base-100
         h-screen overflow-hidden
-        ">
+        `}>
           <Header/>
           {children}
-        </div>
       </body>
     </html>
   );
