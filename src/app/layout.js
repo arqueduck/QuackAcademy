3bcc32fd0ae00a2c33c5cfb62a249c5a94cca4a3
Gustavo-Aal <gustavomarquesaal@gmail.com>
2024-06-19 13:30:20 -0300

    Adicionado meta tag viewport do Tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "QuackAcademy",
   description: "Desenvolvendo seu primeiro site de forma descomplicada",
+  viewport: "width=device-width, initial-scale=1.0"
 };
 
 export default function RootLayout({ children }) {
