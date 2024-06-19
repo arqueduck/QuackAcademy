bd97f1e1ca6e50a32e08267822d3e9e235a3db8c
Gustavo-Aal <gustavomarquesaal@gmail.com>
2024-06-19 14:25:16 -0300

    Atualizado responsividade

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,6 +1,6 @@
 export default function Sidemenu({className, children}){
     return (
-        <ul className={`h-full bg-base-200 menu ${className}`}>
+        <ul className={`hidden h-full bg-base-200 menu lg:block ${className}`}>
             {children}
         </ul>
     )
